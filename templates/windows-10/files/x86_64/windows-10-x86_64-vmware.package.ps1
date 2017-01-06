30ee454648bd593fcfddcddedbf339560fada184
sacres <steven@swatteksystems.com>
2017-01-06 01:18:56 -0500

    Revert "Merge upstream"

--- templates/windows-10/files/x86_64/windows-10-x86_64-vmware.package.ps1
+++ templates/windows-10/files/x86_64/windows-10-x86_64-vmware.package.ps1
@@ -9,8 +9,9 @@ $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a r
 
 if (Test-PendingReboot){ Invoke-Reboot }
 
-Write-BoxstarterMessage "Disabling Sleep timers"
-Disable-PC-Sleep
+Write-BoxstarterMessage "Disabling Hiberation..."
+Set-ItemProperty -Path 'Registry::HKLM\SYSTEM\CurrentControlSet\Control\Power' -Name 'HibernateFileSizePercent' -Value 0
+Set-ItemProperty -Path 'Registry::HKLM\SYSTEM\CurrentControlSet\Control\Power' -Name 'HibernateEnabled' -Value 0
 
 if (-not (Test-Path "A:\NET45.installed"))
 {
